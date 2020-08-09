3e43506773eda40ca59f6d4aba97299522ee7f48
yomna raouf <yomnaraouf99@gmail.com>
2020-08-09 10:55:48 -0700

    Updates

---
+++ precache-manifest.1e1839dc84ef99f295b72f2870210ed2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c31ac753fa4c6f9a2d011d8c111944",
+    "url": "/otlob.git/index.html"
+  },
+  {
+    "revision": "392ae13223914bfbedfa",
+    "url": "/otlob.git/static/css/main.fe988f01.chunk.css"
+  },
+  {
+    "revision": "842e43311250fdffc74f",
+    "url": "/otlob.git/static/js/2.dd383ce9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/otlob.git/static/js/2.dd383ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392ae13223914bfbedfa",
+    "url": "/otlob.git/static/js/main.45482381.chunk.js"
+  },
+  {
+    "revision": "51847cf1324a8a564d25",
+    "url": "/otlob.git/static/js/runtime-main.77371198.js"
+  },
+  {
+    "revision": "04125261e0be032ae9d400618c2ea280",
+    "url": "/otlob.git/static/media/Bag.04125261.svg"
+  },
+  {
+    "revision": "bb1260c1d28c2d9548b8fe8a6859f642",
+    "url": "/otlob.git/static/media/Group-182.bb1260c1.svg"
+  },
+  {
+    "revision": "d3db489b12e24ca26818e333ef4618fe",
+    "url": "/otlob.git/static/media/Group-188.d3db489b.svg"
+  },
+  {
+    "revision": "6d177a5b5d73d00457dd194f6a5d9ecc",
+    "url": "/otlob.git/static/media/Home.6d177a5b.jpg"
+  },
+  {
+    "revision": "01c0a3b63c1caed81e04ab1e41ba61fe",
+    "url": "/otlob.git/static/media/Rectangle-129.01c0a3b6.png"
+  },
+  {
+    "revision": "71d0769de3c5ec7de488c523d68c4df7",
+    "url": "/otlob.git/static/media/Subtraction-14.71d0769d.svg"
+  },
+  {
+    "revision": "ab744626f06b7811af1a2d8089dc182b",
+    "url": "/otlob.git/static/media/Subtraction-17.ab744626.svg"
+  },
+  {
+    "revision": "038cd4c08aa42908c396fb873831ad1f",
+    "url": "/otlob.git/static/media/Subtraction-4.038cd4c0.svg"
+  },
+  {
+    "revision": "0d300ad349705f5995a79bca184da6d6",
+    "url": "/otlob.git/static/media/Union-4.0d300ad3.svg"
+  },
+  {
+    "revision": "ee43a682f3643650ae724ceff9a55d12",
+    "url": "/otlob.git/static/media/arrow.ee43a682.svg"
+  },
+  {
+    "revision": "45d64b5bdd23e8daecfafb6adcb7abf3",
+    "url": "/otlob.git/static/media/emptystate.45d64b5b.svg"
+  },
+  {
+    "revision": "057e0f82ebef4514a10785220f190a93",
+    "url": "/otlob.git/static/media/pic.057e0f82.jpg"
+  }
+]);
